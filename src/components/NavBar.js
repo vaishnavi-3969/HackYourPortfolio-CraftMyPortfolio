9e9ab722e278329b29b77e17d9cbdc4a32dde228
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-06-04 01:11:46 +0530

    updated code

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { FaHome, FaUserCircle, FaSignOutAlt } from 'react-icons/fa';
+import { Button } from 'react-bootstrap';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
+  const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
+
   return (
     <nav className="bg-gray-800 shadow-md">
       <div className="container mx-auto px-8 py-4">
@@ -10,31 +16,57 @@ const NavBar = () => {
               Craft My Portfolio
             </div>
           </div>
-          <div className="flex items-center">
+          <div className="flex items-center space-x-4">
             <a
               href="/"
-              className="text-gray-300 hover:text-white hover:bg-blue-500 px-4 py-2 rounded-md text-base font-medium transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
+              className="text-white hover:bg-blue-500 px-4 py-2 rounded-md text-base font-medium transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
             >
               Home
             </a>
             <a
               href="/"
-              className="text-gray-300 hover:text-white hover:bg-green-500 px-4 py-2 rounded-md text-base font-medium transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
+              className="text-white hover:bg-green-500 px-4 py-2 rounded-md text-base font-medium transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
             >
               Projects
             </a>
             <a
               href="/"
-              className="text-gray-300 hover:text-white hover:bg-purple-500 px-4 py-2 rounded-md text-base font-medium transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
+              className="text-white hover:bg-purple-500 px-4 py-2 rounded-md text-base font-medium transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
             >
               About
             </a>
             <a
               href="/"
-              className="text-gray-300 hover:text-white hover:bg-yellow-500 px-4 py-2 rounded-md text-base font-medium transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
+              className="text-white hover:bg-yellow-500 px-4 py-2 rounded-md text-base font-medium transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
             >
               Contact
             </a>
+            {isAuthenticated && (
+              <p>
+                <a href="/profile" className="text-white hover:text-blue-200">
+                  <FaUserCircle className="nav-icon inline-block mr-1" />
+                  {user.name}
+                </a>
+              </p>
+            )}
+            {isAuthenticated ? (
+              <>
+                <Button
+                  className="login-button text-white animate__animated animate__fadeIn"
+                  onClick={() => logout({ returnTo: window.location.origin })}
+                >
+                  <FaSignOutAlt className="logout-icon" />
+                </Button>
+              </>
+            ) : (
+              <Button
+                className="login-button text-white animate__animated animate__fadeIn"
+                onClick={() => loginWithRedirect()}
+              >
+                <FaUserCircle className="login-icon inline-block mr-1" color='white'/>
+                Log In
+              </Button>
+            )}
           </div>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
+<Auth0Provider
+    domain="dev-2mgbgxm3b0scb3xq.us.auth0.com"
+    clientId="S4zFLUKFSV6o36kAUGrsi4EiHSFuxnff"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
